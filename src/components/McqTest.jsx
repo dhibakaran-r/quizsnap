9fe52a1957a67b73c0cdf410af4e4fcea0adcfc1
dhibakaran-r <rdhibakaran7@gmail.com>
2024-11-20 13:28:36 +0530

    update

--- src/components/McqTest.jsx
+++ src/components/McqTest.jsx
@@ -1,14 +1,24 @@
 import React, { useEffect, useState } from 'react'
-import qes from '../assets/datas/questions.json'
+import qes from '../assets/datas/HTMLBasic.json'
 import { LuClock12 } from "react-icons/lu";
-import { FaHtml5 as Htmlic } from "react-icons/fa";
+import { FaHtml5, FaCss3Alt, FaJsSquare, FaReact, FaCode } from "react-icons/fa";
 import { GrDocumentVerified } from "react-icons/gr";
 import { toast, ToastContainer } from 'react-toastify';
+import { shuffleArray } from '../utils/shuffle';
 
 function McqTest() {
 
     const [timeLeft, setTimeLeft] = useState(30 * 60);
     const [isSubmit, setIsSubmit] = useState(false);
+    const [shuffledQuestions, setShuffledQuestions] = useState([]);
+
+        const icons = {
+            HTML: <FaHtml5 size={50} />,
+            CSS: <FaCss3Alt size={50} />,
+            JavaScript: <FaJsSquare size={50} />,
+            React: <FaReact size={50} />,
+    
+        }
 
     const formatTime = (seconds) => {
         const minutes = Math.floor(seconds / 60);
@@ -16,6 +26,18 @@ function McqTest() {
         return `${minutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
     };
 
+    // shuffle
+    useEffect(() => {
+        if (qes.questions) {
+            const shuffled = shuffleArray(qes.questions).map((question) => ({
+                ...question,
+                options: shuffleArray(question.options),
+            }));
+            setShuffledQuestions(shuffled);
+        }
+    }, []);
+
+    // time display
     useEffect(() => {
         if (timeLeft > 0 && !isSubmit) {
             const timer = setTimeout(() => setTimeLeft(timeLeft - 1), 1000);
@@ -55,18 +77,18 @@ function McqTest() {
     return (
         <div className='w-4/5 ms-16 p-16 flex flex-col gap-40 '>
             {!isSubmit ? (<>
-                {qes.categories.map((que, i) => {
-                    return (
+                {/* {qes.categories.map((que, i) => {
+                    return ( */}
                         <div className='flex flex-col gap-8'>
                             <div className='w-full flex justify-between items-center bg-outlg p-8'>
-                                <div key={i} className='w-40 flex flex-row justify-center items-center gap-2'><Htmlic size={50} /> <span className='font-bold'>{que.category}</span></div>
+                                <div className='w-40 flex flex-row justify-center items-center gap-2'>{icons[qes.category] || <FaCode size={50} />} <span className='font-bold'>{qes.category}</span></div>
                                 <div className='w-40 flex flex-row justify-center items-center gap-2 p-4 bg-secondary rounded-full text-redbg'><LuClock12 size={50} className='clockSpin' /> <span className='text-2xl font-bold'>{formatTime(timeLeft)}</span></div>
                             </div>
                             <div className='flex flex-col justify-around gap-16'>
                                 {/* if({que.levels === "easy"})  */}
                                 {
 
-                                    que.levels.easy.map((q, i) => {
+                                    shuffledQuestions.map((q, i) => {
                                         return (
 
                                             <div key={i} className='flex flex-col justify-around gap-4'>
@@ -89,8 +111,8 @@ function McqTest() {
                                 }
                             </div>
                         </div>
-                    )
-                })}
+                    {/* )
+                })} */}
 
                 <div className='flex justify-center items-center'>
                     <button className="px-60 py-2 flex justify-center items-center gap-2 bg-secondary text-bluebg border rounded-md duration-200 hover:text-primary hover:bg-bluebg hover:border-primary"
