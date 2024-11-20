a247e19a158141cac1a795380296848e8aae6e46
dhibakaran-r <rdhibakaran7@gmail.com>
2024-11-20 15:49:29 +0530

    update

---
+++ src/service/dataStorage.js
@@ -0,0 +1,29 @@
+export const storeData = (data) => {
+  localStorage.setItem("idToken", data);
+};
+
+export const getAuthData = () => {
+  return localStorage.getItem("idToken");
+};
+
+export const getRole = () => {
+  let role = localStorage.getItem("role");
+  let isadmin;
+  if (role === "admin") {
+    isadmin = true;
+  } else {
+    isadmin = false;
+  }
+
+  return isadmin;
+};
+
+const alltokens = (tokens) => {
+  tokens.forEach((tokens) => {
+    localStorage.removeItem(tokens);
+  });
+};
+
+export const removeAuthData = () => {
+  return alltokens(["idToken", "role", "user"]);
+};
